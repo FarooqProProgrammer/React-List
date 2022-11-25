74b11c9a587bc1f903232430729be8414271b969
FarooqProProgrammer <farooqayub646@gmail.com>
2022-11-25 12:38:27 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [text,setText] = useState()
+  const [list,setList] = useState([])
+  
+  const item =(e)=>{
+
+      setText(e.target.value)
+  }
+
+  const add = ()=>{
+    const data = [...list]
+    data.push(text)
+    setList(data)
+  }
+
+
+  const Delete=(index)=>{
+    const data = [...list]
+    data.splice(index,1)
+    setList(data)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+      <div className="center_div">
+        <h1>Todu List</h1>
+        <input  placeholder='Enter Todo' onChange={item}/>
+        <button onClick={add}><i class="fa-solid fa-plus"></i></button>
+
+        <div className='List'>
+        <ul>
+          {
+            list.map((item,index)=>{
+              return (
+                <li>
+              <i onClick={()=> Delete(index)} class="fa-regular fa-circle-xmark"></i>
+               {item}
+            <i class="fa-solid fa-pen-to-square"></i>
+            </li>
+              )
+              
+            })
+          }
+        </ul>
+
+        
+        </div>
+      </div>
     </div>
   );
 }
